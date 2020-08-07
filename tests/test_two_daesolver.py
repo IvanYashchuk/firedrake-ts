a70f153e3ac76beb7eb0da4678e59a242bd635c8
Ivan Yashchuk <ivan.yashchuk@aalto.fi>
2020-08-07 12:34:54 +0300

    black formatting

--- tests/test_two_daesolver.py
+++ tests/test_two_daesolver.py
@@ -18,7 +18,6 @@ def test_two_ts():
     bump = conditional(lt(abs(x[0] - 0.5), 0.1), 1.0, 0.0)
     u.interpolate(bump)
 
-
     problem = firedrake_ts.DAEProblem(F, u, u_t, (0.0, 1.0), bcs=bc)
     solver = firedrake_ts.DAESolver(problem)
 
@@ -29,8 +28,7 @@ def test_two_ts():
     F2 = inner(u_t2, v) * dx + inner(grad(u2), grad(v)) * dx - 1.0 * v * dx
     bc2 = DirichletBC(V2, 0.0, "on_boundary")
 
-
     problem2 = firedrake_ts.DAEProblem(F2, u2, u_t2, (0.0, 1.0), bcs=bc2)
-    solver2 = firedrake_ts.DAESolver(problem2)
+    solver2 = firedrake_ts.DAESolver(problem2)  # noqa: F841
 
     solver.solve()
